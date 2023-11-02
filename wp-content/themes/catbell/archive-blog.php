80b1baa56aaf06d475a4183d541513009ed916a8
ik <nastasja5959@pink.megaegg.ne.jp>
2023-11-02 18:25:03 +0900

    こんどはなんだ？

--- wp-content/themes/catbell/archive-blog.php
+++ wp-content/themes/catbell/archive-blog.php
@@ -1,79 +1,69 @@
 <?php get_header(); ?>
-    <main class="main cntInner inner">
-    <!-- パンくずリスト -->
-        <nav>
-            <ol class="breadcrumbs">
-                <li class="breadcrumbs__item"><a href="index.html" class="breadcrumbs__link">ホーム</a></li>
-                <li class="breadcrumbs__item">ブログ一覧</li>
-            </ol>
-        </nav>
-    <!-- /パンくずリスト -->
 
+<!-- main -->
+    <main class="main cntInner inner">
+    <?php get_template_part('_inc/breadcrumb'); ?>
     <!-- top -->
-    <?php if (have_posts()) : ?>
-        <a href="<?php the_permalink(); ?>" class="archive__cardLink">
-            <div class="top__img">
-            <img src="<?php echo get_field('blog_img'); ?>" alt="<?php echo the_title();?>">
-            </div>
+        <section>
+            <a href="<?php the_permalink(); ?>" class="top__img">
+                <img src="<?php echo get_field('blog_img'); ?>" alt="<?php echo get_the_title(); ?>">
+            </a>
             <div class="top__content">
-                <h2 class="top__ttl"><?php echo the_title(); ?></h2>
-                <div class="top__txt"><?php echo the_content(); ?></div>
+                
+            <?php if ( have_posts() ) : ?>
+                <a href="<?php the_permalink(); ?>">
+                    <h2 class="top__ttl"><?php echo get_the_title(); ?></h2>
+                    <div class="top__txt"><?php echo get_field('blog_text'); ?></div>
+                </a>
                 <div class="top__tagItems">
                     <?php the_tags('<div class="tag top__tag">','</div><div class="tag top__tag">','</div>'); ?>
                 </div>
-                <span class="date top__date"><?php echo the_time('Y,m,d'); ?></span>
+                <span class="date top__date"><?php the_time('Y.m.d') ?></span>
+            <?php endif; wp_reset_postdata(); ?>
+
             </div>
-        </a>
-    <?php endif; ?>
+        </section>
     <!-- /top -->
-    
     <!-- content -->
         <section class="content">
-        <div class="archive">
-            <div class="archive__wrap">
-        
-            <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-            <?php if (!is_first()) : ?>
-                <div class="archive__card">
-                    <a href="<?php the_permalink(); ?>" class="archive__cardLink">
-                        <div class="archive__cardWrap">
-                            <div class="archive__img">
-                                <img src="<?php echo get_field('blog_img'); ?>" alt="<?php the_title(); ?>">
+            <div class="archive">
+                <div class="archive__wrap">
+                <?php if ( have_posts() ) : ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                <?php if (!is_first()) : ?>
+                    <div class="archive__card">
+                        <a href="<?php the_permalink(); ?>" class="archive__cardLink">
+                            <div class="archive__cardWrap">
+                                <div class="archive__img">
+                                    <img src="<?php echo get_field('blog_img'); ?>" alt="<?php echo get_the_title(); ?>">
+                                </div>
+                                <div class="archive__content">
+                                    <span class="date"><?php the_time('Y.m.d') ?></span>
+                                    <div class="archive__ttl"><?php echo get_the_title(); ?></div>
+                                    <div class="archive__txt"><?php echo get_field('blog_text'); ?></div>
+                                </div>
                             </div>
-                            <div class="archive__content">
-                                <span class="date"><?php echo the_time('Y,m,d'); ?></span>
-                                <div class="archive__ttl"><?php the_title(); ?></div>
-                                <div class="archive__txt"><?php the_content(); ?></div>
+                        </a>
+                        <div class="archive__tagItems">
+                            <div class="top__tagItems">
+                                <?php the_tags('<div class="tag top__tag">','</div><div class="tag top__tag">','</div>'); ?>
                             </div>
                         </div>
-                    </a>
-                </div>    
-                <div class="archive__tagItems">
-                    <?php the_tags('<div class="tag top__tag">','</div><div class="tag top__tag">','</div>'); ?>
+                    </div>
+                <?php endif; endwhile; endif; wp_reset_postdata(); ?>
                 </div>
-            <?php endif; ?>
-            <?php endwhile; endif; ?>
-            </div>
-        </div>
-            
-        <div class="pager">
-            <ul class="pager__items">
-                <li class="pager__item is-active"><a href="#" class="pager__link">1</a></li>
-                <li class="pager__item"><a href="#" class="pager__link">2</a></li>
-                <li class="pager__item"><a href="#" class="pager__link">3</a></li>
-                <li class="pager__item"><a href="#" class="pager__link">4</a></li>
-                <li class="pager__item"><a href="#" class="pager__link">5</a></li>
-            </ul>
-        </div>
-    </div>
-            
-       <?php get_template_part('_inc/sidebar'); ?>
 
+                <?php get_template_part('_inc/pager'); ?>
+                
+            </div>
+
+            <?php get_template_part('_inc/sidebar'); ?>
         </section>
     <!-- /content -->
     </main>
+<!-- /main -->
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
+
 </body>
 </html>
