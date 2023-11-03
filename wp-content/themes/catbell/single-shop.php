6c0f871c5dbba8655d7d36bb01bce9f6208a9359
ik <nastasja5959@pink.megaegg.ne.jp>
2023-11-03 09:26:32 +0900

    店舗詳細ページ完了

--- wp-content/themes/catbell/single-shop.php
+++ wp-content/themes/catbell/single-shop.php
@@ -10,36 +10,29 @@
         </ol>
       </nav>
       <!-- breadcrumb__inner inner -->
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/shopDetail/sinjyuku.jpg" alt="">
+      <img src="<?php echo get_field('shop_img'); ?>" alt="<?php the_title(); ?>">
     </div>
   </div><!-- mainvisual -->
 
   <section class="access">
     <div class="inner">
-      <h2 class="section__title">新宿店の基本情報</h2>
+      <h2 class="section__title"><?php the_title(); ?>の基本情報</h2>
       <div class="access__inner">
         <div class="access__text">
           <dl>
             <dt>住所</dt>
-            <dd>東京都新宿区西新宿2-22-2キャットベル</dd>
+            <dd><?php echo get_field('shop_address'); ?></dd>
             <dt>TEL</dt>
-            <dd>22-2222-2222</dd>
+            <dd><?php echo get_field('shop_tell'); ?></dd>
             <dt>営業時間</dt>
-            <dd>平日11:00〜19:30/土日祝11:00〜20:00</dd>
-            <dd>休日：年中無休</dd>
+            <dd><?php echo get_field('shop_hours'); ?></dd>
             <dt>アクセス</dt>
-            <dd>電車でお越しのお客様</dd>
-            <dd>JR線「新宿駅」より徒歩約5分</dd>
-            <dd>西武新宿線「新宿駅」より徒歩約5分</dd>
-            <dd>京王線京王新線・小田急線「新宿駅」より徒歩約10分</dd>
-            <dd>東京メトロ丸ノ内線「新宿駅」より徒歩約10分</dd>
-            <dd>お車でお越しのお客様</dd>
-            <dd>首都高速4号新宿線「新宿I.C.」より約10分</dd>
+            <dd><?php echo get_field('shop_access'); ?></dd>
           </dl>
         </div><!-- access__text -->
 
         <div class="access__map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6480.991848447299!2d139.68712862455956!3d35.689411970769285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd4ca00eef1%3A0x88a16ce9397e1993!2z44CSMTYwLTAwMjMg5p2x5Lqs6YO95paw5a6_5Yy66KW_5paw5a6_77yS5LiB55uu!5e0!3m2!1sja!2sjp!4v1606655019329!5m2!1sja!2sjp"  frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <dd><?php echo get_field('shop_map'); ?></dd>
         </div><!-- access__map -->
       </div><!-- access__inner -->
     </div><!-- inner -->
@@ -47,7 +40,7 @@
 
   <section class="catlist">
     <div class="inner">
-      <h2 class="section__title">新宿店の新入りの猫ちゃん</h2>
+      <h2 class="section__title"><?php the_title(); ?>の新入りの猫ちゃん</h2>
       <ul class="catlist__items">
         <li class="catlist__item">
           <a href="#">
@@ -101,13 +94,12 @@
       <h2 class="section__title">店長よりごあいさつ</h2>
       <div class="concept__inner">
         <div class="concept__left">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/shopDetail/greeting.jpg" alt="">
+        <img src="<?php echo get_field('shop_manager_img'); ?>" alt="<?php the_title(); ?>">
         </div><!-- concept__left -->
 
         <div class="concept__right">
-          <h3 class="concept__subtitle">お客様との出会いとその後の関係をより深めるサポート</h3>
-          <p class="concept__text">こんにちは！CTABELL 新宿店です。当サロンでは、最新の道具と定期的なトリマーの研修を行い、飼い主さまにもペットにも優しいトリミングとサービスをご提供できるように日々努めています。
-            また、健康状態のチェックや皮膚の状態をチェックするのは、トリミングを行う前にとても大切ですので、きちんと確認を行った上で、その子に合った最適なスタイルをご提案させていただきます。ご要望などがありましたら、お気軽にお申し付けください。スタッフ一同常に笑顔で皆さまのご来店をお待ちしています。</p>
+          <h3 class="concept__subtitle"><?php echo get_field('shop_manager_title');  ?></h3>
+          <p class="concept__text"><?php echo get_field('shop_manager_text');  ?></p>
         </div><!-- concept__right -->
       </div><!-- concept__inner -->
     </div><!-- inner -->
@@ -115,7 +107,7 @@
 
   <section class="blog">
     <div class="inner">
-      <h2 class="section__title">新宿店の最新ブログ</h2>
+      <h2 class="section__title"><?php the_title(); ?>の最新ブログ</h2>
 
       <ul class="blog__items">
         <li class="blog__item">
@@ -150,7 +142,7 @@
             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/shopDetail/02.jpg" alt="ブログ記事写真">
             <div class="blog__item__text">
               <p class="date">2022.02.24</p>
-              <h3 class="blog__subtitle">【新宿店】ポイント2倍Day！この機会をお見逃しなく！ポイント2倍Day！この機会をお見逃しなく！ポイント2倍Day！この機会をお見逃しなく！</h3>
+              <h3 class="blog__subtitle">【<?php the_title(); ?>】ポイント2倍Day！この機会をお見逃しなく！ポイント2倍Day！この機会をお見逃しなく！ポイント2倍Day！この機会をお見逃しなく！</h3>
               <p class="blog__text">人とコミュニケーションをとること、物理的に触れたり、間接的に感じたりすることが、今や遠隔で完結できるようになった。もともと「繋がり」という形を持たない結びつきではあるが、あらゆる物事と実際に接点を持つ場面がが減っている中、生身の身体が受け取る感覚はこれからどんなふうに変わっていくのだろうか。人とコミュニケーションをとること、物理的に触れたり、間接的に感じたりすることが、今や遠隔で完結できるようになった。もともと「繋がり」という形を持たない結びつきではあるが、あらゆる物事と実際に接点を持つ場面がが減っている中、生身の身体が受け取る感覚はこれからどんなふうに変わっていくのだろうか</p>
               <a class="blog__item__link" href="#">#ポイントDay</a>
               <a class="blog__item__link" href="#">#ヘルスケア</a>
